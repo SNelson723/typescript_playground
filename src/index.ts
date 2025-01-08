0af864bed6247bde8fbb8abaa07498e3e27fa308
SNelson723 <stephenn649@gmail.com>
2025-01-07 18:52:23 -0600

    Started sandboxing with the Box and working with TypeScript

--- src/index.ts
+++ src/index.ts
@@ -10,15 +10,32 @@ document.body.appendChild(root);
 const main = document.createElement('div');
 const child = document.createElement('p');
 child.innerHTML = 'Hello';
+
 main.appendChild(child);
 root.appendChild(main);
 
 
 import { Box } from './Box';
-import { data } from './data';
+import { data, IData } from './data';
 
 // total units
+const totalUnits = Box(data)
+  .map((x: IData[]) => x.filter((y: IData) => Number(y.units as string) > 0))
+  .map((x: IData[]) => x.filter((y: IData) => y.free != '1'))
+  .map((x: IData[]) => x.filter((y: IData) => y.return_item != '1'))
+  .map((x: IData[]) => x.reduce((total: number, curr: IData) => total += Number(curr.units), 0))
+  .fold((x: number) => x);
+
+console.log('Total Units', totalUnits);
+
 // total unit cost
+const totalUnitCost = Box(data)
+  .map((x: IData[]) => x.filter((y: IData) => Number(y.units) > 0))
+  .map((x: IData[]) => x.filter((y: IData) => y.free != '1'))
+  .fold((x: IData[]) => x)
+
+console.log('Total Unit Cost', totalUnitCost);
+
 // total cases
 // total case cost
 // cost of all purchased items
