06026cc76e7bad37ee5e16e9b6abee0623ebe95a
SNelson723 <stephenn649@gmail.com>
2025-01-07 18:21:56 -0600

    Imported data and Box into index.ts to start sandboxing

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { add } from './modules/math';
 
+
 console.log('you are ready to start coding typescript...');
 
 const root = document.createElement('div');
@@ -11,3 +12,17 @@ const child = document.createElement('p');
 child.innerHTML = 'Hello';
 main.appendChild(child);
 root.appendChild(main);
+
+
+import { Box } from './Box';
+import { data } from './data';
+
+// total units
+// total unit cost
+// total cases
+// total case cost
+// cost of all purchased items
+// total returns
+// total free items
+// total returns
+
