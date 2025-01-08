96e26dfe422ca52cec388a23d41048b911d9a377
SNelson723 <stephenn649@gmail.com>
2025-01-07 19:49:41 -0600

    Finished finding the total cases from the data array using the Box

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,6 @@ document.body.appendChild(root);
 
 const main = document.createElement('div');
 const child = document.createElement('p');
-child.innerHTML = 'Hello';
 
 main.appendChild(child);
 root.appendChild(main);
@@ -42,21 +41,30 @@ console.log('Total Unit Cost', totalUnitCost);
 document.getElementById('totalUnitCost')!.innerHTML = `Total Unit Cost: ${totalUnitCost}`
 
 // total cases
+const totalCases = Box(data)
+  .map((x: IData[]) => x.filter((y: IData) => Number(y.cases) > 0))
+  .map((X: IData[]) => X.filter((y: IData) => y.free != '1' && y.return_item != '1'))
+  .map((x: IData[]) => x.reduce((total: number, curr: IData) => total += Number(curr.cases), 0))
+  .fold((x: number) => x);
+
+  console.log('Total Cases', totalCases);
+  document.getElementById('totalCases')!.innerHTML = `Total Cases: ${totalCases}`;
+
 // total case cost
+
+
 // cost of all purchased items
 // total returns
 // total free items
+
 // total returns
-const totalReturns = Box(data)
-  // .map
+// const totalReturns = Box(data)
+//   .map((x: IData[]) => x.filter((y: IData) => y.return_item == '1'))
+//   .trace()
+  // .fold((x: IData[]) => x)
 
 
 // total purchased
-const trace = <T>(x: T) => {
-  console.log(x);
-  return x;
-};
-
 const totalPurchased = Box(data)
   .map((x: IData[]) => x.filter((y: IData) => y.free != '1'))
   .map((x: IData[]) => x.map((y: IData) => ({
@@ -66,7 +74,6 @@ const totalPurchased = Box(data)
     upc: y.upc
   })))
   .map((x: IData[]) => x.reduce((acc, cur) => cur.return_item ? acc - +cur.cases + +cur.units : +cur.cases + +cur.units, 0))
-  // .trace()
   .map((x: number) => `$${x.toFixed(2)}`)
   .fold((x: string) => x)
 
